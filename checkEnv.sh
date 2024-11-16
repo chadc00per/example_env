19cd9cd61a47f29ebb039d247d056d55bf270230
Chad Cooper <chadcoop@me.com>
2024-11-16 06:32:17 -0500

    Update checkEnv.sh

--- checkEnv.sh
+++ checkEnv.sh
@@ -1,5 +1,12 @@
+#!/bin/bash
+
+# Create a .example.env file from the .env file
+# Export the environment variables
+# Check if the required keys are present in the environment
+
+source .env
+
 create_example_env() {
-    # Create or overwrite .example.env file with keys but no values
     > .example.env
     while IFS= read -r line; do
         if [[ $line == *"="* ]]; then
@@ -12,8 +19,48 @@ create_example_env() {
 }
 
 function export_env_vars() {
-    export $(grep -v '^#' .env | xargs)
+    while IFS= read -r line; do
+        if [[ $line == *"="* ]] && [[ $line != \#* ]]; then
+            key=$(echo $line | cut -d '=' -f 1)
+            value=$(echo $line | cut -d '=' -f 2-)
+            export "$key=$value"
+        fi
+    done < .env
+    echo "ENV variables exported!"
+}
+
+function read_env_file() {
+    grep -v -e '^#' -e '^$' .example.env
+}
+
+function check_env_keys() {
+    local missing_keys=()
+    local found_keys=()
+    
+    while IFS= read -r line; do
+        if [[ $line == *"="* ]]; then
+            key=$(echo $line | cut -d '=' -f 1)
+            if [[ -z "${!key}" ]]; then
+                missing_keys+=("$key")
+            else
+                found_keys+=("$key")
+            fi
+        fi
+    done < <(read_env_file)
+    
+    if [ ${#found_keys[@]} -eq 0 ]; then
+        echo "No ENV keys were found"
+    else
+        echo "Verifying ENV..."
+    fi
+    
+    if [ ${#missing_keys[@]} -eq 0 ]; then
+        echo "ENV verified!"
+    else
+        echo "Missing ENV keys: ${missing_keys[@]}"
+    fi
 }
 
 create_example_env
 export_env_vars
+check_env_keys
